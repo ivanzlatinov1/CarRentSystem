c1d28b26b4eb372867182c7c8b56f1977bfe00eb
ivanzlatinov1 <ivanzlatinov006@gmail.com>
2025-03-22 18:48:25 +0200

    Added User Seeder with Roles

---
+++ CarRentSystem.Data/Seeders/UserSeeder.cs
@@ -0,0 +1,76 @@
+﻿using CarRentSystem.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarRentSystem.Data.Seeders
+{
+    public static class UserSeeder
+    {
+        public static async Task Initialize(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<User>>();
+
+            await SeedRoles(roleManager);
+            await SeedUsers(userManager);
+        }
+
+        private static async Task SeedUsers(UserManager<User> userManager)
+        {
+            // Seeding the admin
+            var adminUser = new User
+            {
+                UserName = "admin_user",
+                Email = "admin@admin.com",
+                EmailConfirmed = true,
+                FirstName = "Admin",
+                LastName = "User",
+                EGN = "0000000000",
+                PhoneNumber = "1234567890"
+            };
+
+            string adminPassword = "Admin#123";
+            await SeedUser(adminUser, adminPassword, "Admin", userManager);
+
+            // Seeding a regular user
+            var regularUser = new User()
+            {
+                UserName = "user_default",
+                Email = "user@user.user.com",
+                EmailConfirmed = true,
+                FirstName = "Test",
+                LastName = "User",
+                EGN = "0000000000",
+                PhoneNumber = "1234567890"
+            };
+            string regularPassword = "User#123";
+            await SeedUser(regularUser, regularPassword, "User", userManager);
+        }
+
+        private static async Task SeedUser(User user, string password, string roleName,
+            UserManager<User> userManager)
+        {
+            User? userInfo = await userManager.FindByEmailAsync(user.Email);
+            if (userInfo == null)
+            {
+                var created = await userManager.CreateAsync(user, password);
+                if (created.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(user, roleName);
+                }
+            }
+        }
+        private static async Task SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            string[] roleNames = ["Admin", "User"];
+            foreach (var role in roleNames)
+            {
+                bool roleExist = await roleManager.RoleExistsAsync(role);
+                if (!roleExist)
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+    }
+}
